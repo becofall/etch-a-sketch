fb8a2de7c64c023d3307dd36f25c348fda720cfd
becofall <becofall@gmail.com>
2022-10-03 14:32:32 -0500

    Add change color when hover funcionality

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-let grid = document.getElementById("container");
-let cell = document.createElement("div");
+let grid = document.querySelector("#container");
 
-function makeSquares(num = 16) {
+
+function makeSquares(num = 32) {
   for (let i = 0; i < num; i++) {
     let row = grid.appendChild(document.createElement("div"));
     row.className = "row";
     for (let j = 0; j < num; j++) {
-      let square = document.createElement("div");
-      square.className = "square";
-      row.appendChild(square);
+      let cell = document.createElement("div");
+      cell.className = "cell";
+      row.appendChild(cell);
     }
   }
 }
 
+//console.log(square);
+
 makeSquares();
 
+let square = document.querySelectorAll("div");
 
+document.addEventListener("mouseover", function(e) {
+  if(e.target.classname == "cell") {
+    e.target.style.backgroundColor = "black";
+  }
+});
+
+function clearGrid() {
+  while(grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+  makeSquares();
+}
