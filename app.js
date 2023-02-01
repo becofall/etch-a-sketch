a86d771400d01a3ff9772110106c5556be1747ad
becofall <becofall@gmail.com>
2023-02-01 15:06:41 -0600

    Added hover and click effect in black and color

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ function makeSquares(size) {
   for(let i = 0; i < numDivs; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.style.backgroundColor = color;
-    // cell.addEventListener("mouseover", colorDiv);
+    cell.addEventListener("mouseover", cellColor);
+    cell.addEventListener("click", cellColor);
     grid.insertAdjacentElement("beforeend", cell);
   }
 }
@@ -28,14 +28,18 @@ function gridSizePrompt() {
     clearGrid();
     makeSquares(input);
    } else if (input == "") {
-    prompt("Please provide a number");
+    prompt("Please provide a number between 1-100");
    } else {
     prompt("Must be a number between 1 - 100");
    }
 }
 
 function cellColor() {
-
+  if(color == "black") {
+    this.style.backgroundColor = "black";
+  } else {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  }
 }
 
 rightKnob.addEventListener("click", () => {
@@ -50,25 +54,14 @@ rightKnob.addEventListener("click", () => {
 leftKnob.addEventListener("click", () => {
   let size = gridSizePrompt();
   makeSquares(size);
-  // window.alert("testing");
 });
-
-
-document.addEventListener("mouseover", function(e) {
-  if(e.target.classname == "cell") {
-    e.target.style.backgroundColor = "black";
-  }
-});
-
 
 function clearGrid() {
   grid.innerHTML = "";
-  // makeSquares(16);
 }
 
 resetGrid.addEventListener("click", () => {
   location.reload();
-  // window.alert("testing");
 });
 
 
@@ -76,92 +69,8 @@ resetGrid.addEventListener("click", () => {
 
 //TODO
 //Reset grid DONE
-//Grid size functionality
-//Color mode functionality
+//Grid size functionality DONE
+//Color mode functionality DONE
 //Mobile accessibility
+//Footer DONE
 
-// function makeSquares(num) {
-
-// }
-
-// function makeCell(num) {
-//   num = num || 10;
-//   for(let i = 0; i < num; i++) {
-//     cell = document.createElement("div");
-//     cell.classList.add("cell");
-//     grid.appendChild(cell);
-    
-//   }
-//   // let cellWidth = 640/num;
-//   // let cellHeight = 640/num;
-//   // document.cell.style.width = cellWidth;
-//   // document.cell.style.height = cellHeight;
-// }
-
-// function makeSquares(row, col) {
-//   grid.style.setProperty("--rows", row);
-//   grid.style.setProperty("--cols", col);
-//   for(let i = 0; i < row; i++) {
-//     makeCell(col);
-//   }
-  
-// }w.className = "row";
-//     for (let j = 0; j < num; j++) {
-//       let cell = document.createElement("div");
-//       cell.className = "cell";
-//       row.appendChild(cell);
-//     }
-//   }
-//   let cellWidth = 640/num;
-//   let cellHeight = 640/num;
-//   let cellStyle = document.querySelector(".cell");
-//   cellStyle.style.width = cellWidth + "px";
-//   cellStyle.style.height = cellHeight + "px";
-//   // document.cell.style.width = cellWidth;
-//   // document.cell.style.height = cellHeight;
-//   // document.button.style.backgroundColor = "purple";
-// }
-
-// function makeSquares(num) {
-//   num = num || 10;
-//   for (let rows = 0; rows < num; rows++) {
-//     for (let columns = 0; columns < num; columns++) {
-//       grid.appendChild(cell);
-//     };
-//   };
-//   let cellWidth = 640/num;
-//   let cellHeight = 640/num;
-//   document.cell.style.width = cellWidth;
-//   document.cell.style.height = cellHeight;
-// }n makeSquares (num) {
-//   for (let i = 0; i < num; i++) {
-//     let row = grid.appendChild(document.createElement("div"));
-//     row.className = "row";
-//     for (let j = 0; j < num; j++) {
-//       let cell = document.createElement("div");
-//       cell.className = "cell";
-//       row.appendChild(cell);
-//     }
-//   }
-//   let cellWidth = 640/num;
-//   let cellHeight = 640/num;
-//   let cellStyle = document.querySelector(".cell");
-//   cellStyle.style.width = cellWidth + "px";
-//   cellStyle.style.height = cellHeight + "px";
-//   // document.cell.style.width = cellWidth;
-//   // document.cell.style.height = cellHeight;
-//   // document.button.style.backgroundColor = "purple";
-// }
-
-// function makeSquares(num) {
-//   num = num || 10;
-//   for (let rows = 0; rows < num; rows++) {
-//     for (let columns = 0; columns < num; columns++) {
-//       grid.appendChild(cell);
-//     };
-//   };
-//   let cellWidth = 640/num;
-//   let cellHeight = 640/num;
-//   document.cell.style.width = cellWidth;
-//   document.cell.style.height = cellHeight;
-// }
